fac1af0e842f1db66e4f617989ce1ccd19d8845d
rinat <deez@nut.com>
2025-09-30 20:45:40 +0500

    the beginning of apoj

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
-import time
 from time import localtime, strftime
 from pydub import AudioSegment
-
-sound = AudioSegment.from_file("samples/shit.mp3", format="mp3")
-reversed_sound = sound.reverse()
-nice_try = reversed_sound + sound
-nice_try.export("app/apoj.mp3", format="mp3")
-
+import os
+samples = os.listdir("/samples")
+nice_try = AudioSegment.empty()
+for sample in samples:
+    if sample[-4:] != ".mp3":
+        print(f"sample = {sample} is not mp3")
+        continue
+    sound = AudioSegment.from_file(f"/samples/{sample}", format="mp3")
+    reversed_sound = sound.reverse()
+    nice_try += reversed_sound + sound
+nice_try.export("/app/apoj.mp3", format="mp3")
 print(strftime("%H-%M-%S: shit done\n", localtime()))
-
